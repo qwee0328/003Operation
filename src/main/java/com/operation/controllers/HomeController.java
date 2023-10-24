61e8f8ccf7261afa3d73096da2ddb638d4cc54c9
qwee0328 <qwee0328@naver.com>
2023-10-25 01:09:19 +0900

    dao 업데이트

--- src/main/java/com/operation/controllers/HomeController.java
+++ src/main/java/com/operation/controllers/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
--- src/main/java/com/operation/dao/MemberDAO.java
+++ src/main/java/com/operation/dao/MemberDAO.java
@@ -1,5 +1,8 @@
 package com.operation.dao;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class MemberDAO {
 
 }
