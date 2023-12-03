accb8098a5c0cb35cb97b5f4eaba15c99c996777
Jeen <eyungenius@gmail.com>
2023-12-04 03:18:43 +0900

    키오스크 DB 데이터 삽입 후 DB 내용 키오스크 페이지에 출력하도록 구현

--- src/main/java/com/operation/services/KioskService.java
+++ src/main/java/com/operation/services/KioskService.java
@@ -1,14 +1,25 @@
 package com.operation.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.operation.dao.KioskDAO;
+import com.operation.dto.KioskDTO;
 
 @Service
 public class KioskService {
 	@Autowired
 	private KioskDAO dao;
 	
+	// 키오스크 목록 불러오기
+	public List<KioskDTO> selectAll(int is_game){
+		return dao.selectAll(is_game);
+	}
 	
+	// 키오스크 상세 정보 불러오기
+	public KioskDTO selectById(int id) {
+		return dao.selectById(id);
+	}
 }
