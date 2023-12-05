534044d436dc70ff65d8c4da19f74e6a728488f7
Jeen <eyungenius@gmail.com>
2023-12-06 02:22:50 +0900

    코드 한줄 줄임..

--- src/main/resources/static/js/kiosk/kiosk.js
+++ src/main/resources/static/js/kiosk/kiosk.js
@@ -7,11 +7,10 @@ $(document).ready(function(){
 		type:"post"
 	}).done(function(resp){
 		for(let i=0; i<resp.length; i++){
-			let min=0;
 			let sec=resp[i].play_time;
 			let play_time = sec+"초";
 			if(sec>60) {
-				min = Math.floor(sec/60);
+				let min = Math.floor(sec/60);
 				sec%=60;
 				play_time=min+"분 "+sec+"초";
 			} 
