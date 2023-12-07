e385e5bb0f74baad9566d05930d18215d032a307
Jeen <eyungenius@gmail.com>
2023-12-08 05:09:38 +0900

    qna 작성 시 이미지/파일 처리 구현

--- src/main/resources/static/js/qna/viewQna.js
+++ src/main/resources/static/js/qna/viewQna.js
@@ -1,15 +1,56 @@
+let deleteFileList = [];
+let deleteExisingFileList = [];
+
 $(document).ready(function(){
+
+	// 답변 작성 시에 파일 삽입 시 목록 출력
+	$("#fileInput").on("change",function(e){
+		$(".fileNameList").html("");
+		deleteFileList = [];
+		deleteExisingFileList = [-1];
+		if(e.target.files.length>=6){
+			alert("파일은 최대 5개까지 올릴 수 있습니다.");
+		}
+		for(let i=0; i<e.target.files.length; i++){
+			if(i==5) break;
+			let fileNameTag = $("<div>").attr("class","fileNameTag d-flex");
+			let fileName = $("<div>").attr("class","fileName").text(e.target.files[i].name);
+			let fileIcon = $("<div>").attr("class","ml5").html(`<i class='fa-solid fa-xmark deleteFileBtn' data-id=${i}></i>`)
+			$(".fileNameList").append(fileNameTag.append(fileName).append(fileIcon))		
+		}
+	});
 	
 });
 
-let deleteFileList = [];
-let deleteExisingFileList = [];
+// 파일 삭제
+$(document).on("click",".deleteFileBtn",function(){
+	
+	if($(this).attr("data-id") == undefined){ // 수정중 기존 파일 삭제
+		deleteExisingFileList.push(parseInt($(this).attr("data-seq")));
+	}else{ // 새로 등록한 파일 삭제
+		deleteFileList.push(parseInt($(this).attr("data-id")));
+	}
+	
+	$(this).closest(".fileNameTag").remove();
+});
+
+
 	
 // 목록으로
 $(document).on("click",".goList",function(){
+	// 수정중이었으면 삽입된 이미지 다시 삭제
+	$.ajax({
+		url: "/board/deleteImage",
+		type: "POST",
+		traditional: true,
+		data: { srcList : insertImgs }
+	});
+		
 	location.href="/qna/listBoard";
 });
 
+
+	
 // 답변 작성
 $(document).on("click",".write",function(){
 	// 내용 입력 검사
